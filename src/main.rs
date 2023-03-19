f4768e97d19827ec8acd3cc5c5103c1acd15ce1d
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-03-20 02:02:51 +0900

    add: command line options

--- src/main.rs
+++ src/main.rs
@@ -13,34 +13,56 @@ mod test;
 use std::fs::File;
 use std::io::prelude::*;
 
+use rustop::opts;
+
+fn print_version() {
+    println!("Fukutsu-COBOL version 0.1.0");
+    println!("Copyright (C) 2023 Yutaro Sakamoto");
+}
+
 fn main() -> std::io::Result<()> {
-    let sample_source = r#"
-    identification division.
-    program-id. hello.
-    environment division.
-    DATA division.
-    Working-storage section.
-    01 ab PIC xx value "ab".
-    01 cd pic xx value "cd".
-    procedure division.
-    move ab to cd.
-    DisPlay cd.
-    DisPlay ab."#;
-    let ast = parser::CobolProgramParser::new()
-        .parse(sample_source)
-        .expect("[Error] parse error");
-    let data_description_root_node = DataDescription {
-        level_number: 0,
-        entry_name: "#!@dummy@!#",
-        description_clauses: Vec::new(),
-    };
-    let abstract_code =
-        gen_abstract_code::generate_abstract_code(&ast, &data_description_root_node)
-            .expect("[Error] code geenration error");
-    let js_code = gen_code::js::generate_code(&abstract_code);
+    let (args, _) = opts! {
+        opt version:bool, desc: "Display the version of Fukutsu-COBOL";
+        param infile:Option<String>, desc:"Input file name.";
+        param outfile:Option<String>, desc:"Output file name.";
+    }
+    .parse_or_exit();
 
-    let mut file = File::create("a.js")?;
-    file.write_all(js_code.as_bytes())?;
+    if (args.version) {
+        print_version();
+        return Ok(());
+    }
 
-    Ok(())
+    match args.infile {
+        None => {
+            print_version();
+            Ok(())
+        }
+        Some(ref infile) => {
+            let source = std::fs::read_to_string(infile)
+                .expect(format!("[Error] input file `{}` not found", infile).as_str());
+            let ast = parser::CobolProgramParser::new()
+                .parse(source.as_str())
+                .expect("[Error] parse error");
+            let data_description_root_node = DataDescription {
+                level_number: 0,
+                entry_name: "#!@dummy@!#",
+                description_clauses: Vec::new(),
+            };
+            let abstract_code =
+                gen_abstract_code::generate_abstract_code(&ast, &data_description_root_node)
+                    .expect("[Error] code geenration error");
+            let js_code = gen_code::js::generate_code(&abstract_code);
+
+            let file_path = match args.outfile {
+                Some(file) => file,
+                None => format!("{}.js", ast.identification_division.program_id.to_string()),
+            };
+
+            let mut file = File::create(file_path)?;
+            file.write_all(js_code.as_bytes())?;
+
+            Ok(())
+        }
+    }
 }
---
+++ test/hello.cbl
@@ -0,0 +1,11 @@
+       identification division.
+       program-id. hello.
+       environment division.
+       DATA division.
+       Working-storage section.
+       01 ab PIC xx value "ab".
+       01 cd pic xx value "cd".
+       procedure division.
+       move ab to cd.
+       DisPlay cd.
+       DisPlay ab.

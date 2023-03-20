132249793f4e8e6a99e21f80bd1e4afc67d4842a
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-03-21 04:15:46 +0900

    update: Update CI

--- test/www/index.js
+++ test/www/index.js
@@ -1,8 +1,8 @@
-//const wasm = require("./fcbl-web");
+
 import * as wasm from "fukutsu-cobol";
 let core = wasm.CobolCore.new_by_string("hello_world");
-let field_ab = core.register_field(0, 5, wasm.CobolFieldType.Alphanumeric, 0, 0, wasm.FLAG_NONE, "");
-let field_cd = core.register_field(5, 5, wasm.CobolFieldType.Alphanumeric, 0, 0, wasm.FLAG_NONE, "");
-core.move_field(field_ab, field_cd);
-console.log(core.field_as_string(field_cd));
-console.log(core.field_as_string(field_ab));
+let field_ab = core.register_field (0, 5, wasm.CobolFieldType.Alphanumeric, 0, 0, wasm.FLAG_NONE, "");
+let field_cd = core.register_field (5, 5, wasm.CobolFieldType.Alphanumeric, 0, 0, wasm.FLAG_NONE, "");
+core.move_field (field_ab, field_cd);
+console.log (core.field_as_string (field_cd));
+console.log (core.field_as_string (field_ab));

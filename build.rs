427e68588a43d7b76b4bb6a9f9bd7910d48ef640
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-03-23 02:30:20 +0900

    add: Add a sample gRPC template

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 extern crate lalrpop;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     lalrpop::process_root().unwrap();
+    tonic_build::compile_protos("grpc/fcbl-core.proto")?;
+    Ok(())
 }
